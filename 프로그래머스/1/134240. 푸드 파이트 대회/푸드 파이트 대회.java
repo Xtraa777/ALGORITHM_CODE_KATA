6e307eebe36fdefb413c1e489d9c297a14847948
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2023-11-20 09:41:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.42 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        int num = 0;
+        // a + 0 + a 로 답?
+            
+        for(int i = 1; i < food.length; i++) {
+            if(food[i] / 2 != 0) {
+                num = food[i] / 2;
+                
+                for(int j = 1; j <= num; j++) {
+                    answer += String.valueOf(i);
+                }
+            }
+        }
+        
+        answer += String.valueOf(0);
+        
+        for(int i = answer.length() - 2; i >= 0; i--) {
+            answer += String.valueOf(answer.charAt(i));
+        }
+        
+        return answer;
+    }
+}
