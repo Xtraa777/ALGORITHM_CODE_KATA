59fa080df8af2ca738c0f204e9cf0e946edb046b
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2023-11-28 14:21:57 +0900

    [level 1] Title: 덧칠하기, Time: 1.46 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,6 +1,17 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
+        int answer = 1;
+        int start = section[0];
+        
+        for (int i = 0; i < section.length; i++) {
+            if (start + m > section[i]) {
+                continue;
+            }
+            
+            start = section[i];
+            answer++;
+        }
+        
         return answer;
     }
 }
