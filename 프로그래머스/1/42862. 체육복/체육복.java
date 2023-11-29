9ae39a4c2e86011dfaa31c79849ec042b4be5a1c
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2023-11-30 02:48:08 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        // 중첩 for문으로 안풀림 -> 그러면 그냥 새로운 배열을 만들어서 count해주면?
+        // 문제를... 잘... 읽자...
+        int[] student = new int[n];
+        
+        for (int lost_std : lost) {
+            student[lost_std - 1]--;
+        }
+        for (int reserve_std : reserve) {
+            student[reserve_std - 1]++;
+        }
+        
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] == -1) {
+                if (i > 0 && student[i - 1] == 1) {
+                    
+                    student[i] = student[i - 1] = 0;
+                } else if (i < student.length -1 && student[i + 1] == 1) {
+                    
+                    student[i] = student[i + 1] = 0;
+                } else {
+                    answer--;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
