f936f1a5ffb52b32aaf499895d117071cf5ce501
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2023-12-27 11:29:32 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        // want[i] == number[i]
+        // want[i], number[i] : map(want[i], number[i])와 같다
         
         for (int i = 0; i < discount.length - 9; i++) { // 시작 날짜, 할인 품목
             int j = 0;
