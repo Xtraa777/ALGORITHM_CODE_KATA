df92a204343ceb5fdcdaf8c837b41e4652d28f43
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-03-01 20:52:21 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.13 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int maxCount = 0;
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : array) {
+            int count = map.getOrDefault(num, 0) + 1;
+            if (count > maxCount) {
+                maxCount = count;
+                answer = num;
+            } else if (count == maxCount) {
+                answer = -1;
+            }
+            map.put(num, count);
+        }
+        return answer;
+    }
+}
