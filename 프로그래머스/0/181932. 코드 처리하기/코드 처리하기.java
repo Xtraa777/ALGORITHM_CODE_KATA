a0d400b0775a6c4fb95b637e60eb8b3d9c1ddf22
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-03-06 17:17:13 +0900

    [level 0] Title: 코드 처리하기, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -5,31 +5,19 @@ class Solution {
         int mode = 0;
         
         for (int i = 0; i < c_code.length; i++) {
-            if (mode == 0) {
-                if (c_code[i] != '1') {
-                    if (i % 2 == 0) {
-                        ret += String.valueOf(c_code[i]);
-                    }
-                }
-                if (c_code[i] == '1') {
-                    mode = 1;
-                }
-            } else {
-                if (c_code[i] != '1') {
-                    if (i % 2 != 0) {
-                        ret += String.valueOf(c_code[i]);
-                    }
-                }
-                if (c_code[i] == '1') {
-                    mode = 0;
-                }
+            if (c_code[i] == '1') {
+                mode = mode == 0 ? 1 : 0;
+            } else if (i % 2 == mode) {
+                ret += c_code[i];
             }
-        }
+        
+        } 
         
         if (ret.equals("")) {
-            ret = "EMPTY";
+            return "EMPTY";
+        } else {
+            return ret;
         }
         
-        return ret;
     }
 }
