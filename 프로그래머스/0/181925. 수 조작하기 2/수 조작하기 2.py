d85b07f0fcff5eaebf41e66f977c6d416886b9ac
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-12-05 19:01:46 +0900

    [level 0] Title: 수 조작하기 2, Time: 26.28 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -3,10 +3,7 @@ def solution(numLog):
     
     dict = {1: 'w', -1: 's', 10: 'd', -10: 'a'}
     
-    for i in range(len(numLog)):
-        try:
-            answer += dict[numLog[i+1] - numLog[i]]
-        except:
-            break
+    for i in range(1, len(numLog)):
+            answer += dict[numLog[i] - numLog[i-1]]
     
     return answer
