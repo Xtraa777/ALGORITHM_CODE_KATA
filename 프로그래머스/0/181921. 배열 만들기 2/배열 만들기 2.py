cf4d45741c3c14dac28d3595bd4bb2b641538b7e
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-12-06 19:58:52 +0900

    [level 0] Title: 배열 만들기 2, Time: 247.68 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,16 @@
+def solution(l, r):
+    answer = []
+    
+    for i in range(l, r+1):
+        check = True
+        for s in str(i):
+            if s != '5' and s != '0':
+                check = False
+                break
+        if check:
+            answer.append(i)
+            
+    if not answer:
+        return [-1]
+            
+    return answer
