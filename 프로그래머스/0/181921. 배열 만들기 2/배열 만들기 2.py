c82917261690a6784aca512cf7a48db4eac4ae2e
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-12-06 20:12:20 +0900

    [level 0] Title: 배열 만들기 2, Time: 125.80 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,13 +1,10 @@
 def solution(l, r):
     answer = []
     
-    for i in range(l, r+1):
-        check = True
-        for s in str(i):
-            if s != '5' and s != '0':
-                check = False
-                break
-        if check:
+    l -= l % 5
+    
+    for i in range(l, r+1, 5):
+        if not set(str(i)) - set(['0', '5']):
             answer.append(i)
             
     return answer if answer else [-1]
