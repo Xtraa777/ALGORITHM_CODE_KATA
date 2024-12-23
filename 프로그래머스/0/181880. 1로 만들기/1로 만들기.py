541f7605acb133682000831d700eddedc3d00af8
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-12-23 17:02:36 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -3,11 +3,7 @@ def solution(num_list):
     
     for i in num_list:
         while not i == 1:
-            if i % 2 == 1:
-                i = (i - 1) // 2
-                answer += 1
-            else:
-                i //= 2
-                answer += 1
+            i //= 2
+            answer += 1
     
     return answer
