bd5985887db7464d649f268146a73b478cbaa53d
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2024-12-24 11:47:40 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,9 +1,5 @@
 def solution(angle) :
-    if 0 < angle < 90 :
-        return 1
-    elif angle == 90 :
-        return 2
-    elif 90 < angle < 180 :
-        return 3
-    else :
-        return 4
+    if angle <= 90 :
+        return 2 if angle == 90 else 1        
+    elif 90 < angle:
+        return 4 if angle == 180 else 3
