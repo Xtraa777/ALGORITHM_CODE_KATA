0954c01780dea432afb69ddb9a6e1dec061288e3
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-01-05 01:35:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,9 @@
+def solution(emergency):
+    answer = []
+    
+    desc = sorted(emergency, reverse = True)
+    descDict = {key: value + 1 for value, key in enumerate(desc)}
+
+    answer = [descDict[i] for i in emergency]
+    
+    return answer
