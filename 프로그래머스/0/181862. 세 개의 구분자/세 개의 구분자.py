72f8bc473cc605d9df829d833f9a1c2e69c33ff9
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-01-04 19:06:52 +0900

    [level 0] Title: 세 개의 구분자, Time: 10.50 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,3 +1,3 @@
 def solution(myStr):
-    ans = [i for i in myStr.replace('b', 'a').replace('c', 'a').split('a') if i]
-    return ans if ans else ["EMPTY"]
+    answer = myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split()
+    return answer if answer else ['EMPTY']
