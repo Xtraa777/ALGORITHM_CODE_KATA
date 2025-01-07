1e2ee62e7f0d022eec1958b9b18865bb546670c4
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-01-08 02:13:53 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,8 +1,4 @@
 def solution(rsp):
-    answer = ''
     rspDict = {"2":"0", "0":"5", "5":"2"}
     
-    for i in rsp:
-        answer += rspDict.get(i)
-    
-    return answer
+    return ''.join([rspDict.get(i) for i in rsp])
