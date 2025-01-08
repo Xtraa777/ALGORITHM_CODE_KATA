3aa15c371a761d6c1f8623fa48b6650c466bd6f7
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-01-09 00:37:48 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,8 +1,3 @@
 def solution(my_string, indices):
-    answer = ''
     
-    for i in range(len(my_string)):
-        if i not in indices:
-            answer += my_string[i]
-    
-    return answer
+    return ''.join([i for idx, i in enumerate(my_string) if idx not in indices])
