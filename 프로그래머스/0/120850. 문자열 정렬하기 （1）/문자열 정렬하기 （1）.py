775cd26faaaf86d157971f9626316edd95ef8fb0
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-02-04 23:18:09 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    
+    answer = []
+    
+    for c in my_string:
+        if c.isdigit():
+            answer.append(int(c))
+    
+    answer.sort()
+    
+    return answer
