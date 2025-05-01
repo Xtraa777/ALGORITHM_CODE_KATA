ad0b30c672d91b902d2617203072b11669af198f
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-05-01 18:40:20 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+top = -1
+
+for _ in range(n):
+    temp = list(map(str, input().split()))
+    if temp[0] == 'push':
+        stack.append(int(temp[1]))
+        top += 1
+    elif temp[0] == 'pop':
+        if stack:
+            print(stack.pop())
+            top -= 1
+        else:
+            print(-1)
+    elif temp[0] == 'size':
+        print(len(stack))
+    elif temp[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif temp[0] == 'top':
+        if top != -1:
+            print(stack[top])
+        else:
+            print(-1)
