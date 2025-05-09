f1cb22eae72f8c86661b8d46fe15c2589f78ca2f
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-05-09 11:57:12 +0900

    [level 1] Title: 실패율, Time: 15.37 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,15 +1,17 @@
 from collections import Counter
 
 def solution(N, stages):
-    player = Counter(stages) # {스테이지번호: 멈춘 플레이어 수}
-    total_player = len(stages)
+    answer = []
+    
+    players = Counter(stages)
+    total_p = len(stages)
     
     fails = {}
     for i in range(1, N + 1):
-        if player[i] == 0:
+        if players[i] == 0:
             fails[i] = 0
         else:
-            fails[i] = player[i] / total_player
-            total_player -= player[i]
-    
+            fails[i] = players[i] / total_p
+            total_p -= players[i]
+
     return sorted(fails, key=lambda x : fails[x], reverse=True)
