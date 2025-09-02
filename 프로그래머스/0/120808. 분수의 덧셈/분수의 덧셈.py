bcaafd74b88e4280af589a338c6789104a7dcc45
Xtraa777 <141902464+Xtraa777@users.noreply.github.com>
2025-09-02 11:20:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,10 +1,8 @@
 import math
 
 def solution(numer1, denom1, numer2, denom2):
-    denom = denom1 * denom2
     numer = (numer1 * denom2) + (numer2 * denom1)
-    
-    num = math.gcd(denom, numer)
+    denom = denom1 * denom2
+    num = math.gcd(numer, denom)
     answer = [numer//num, denom//num]
-    
     return answer
